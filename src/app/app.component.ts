344a09cbde9015967dcc747aee151e586a01b12a
Javier Leon <dev.javierleon@gmail.com>
2021-03-29 16:56:08 -0500

    two

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // title = 'angular-ud-c1-p03-pipes';
   name = 'Walter Bishop';
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
