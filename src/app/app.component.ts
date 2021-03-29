96b3656ce94c0a8f79ac49131afc37eed515db51
Javier Leon <dev.javierleon@gmail.com>
2021-03-29 17:50:16 -0500

    three

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,6 @@ export class AppComponent {
   // title = 'angular-ud-c1-p03-pipes';
   name = 'Walter Bishop';
   array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  fringe = ['Precognition', 'Psichokinesis', 'Teleportation', 'Nanotechnology', 'Artificial Intelligence', 'Precognition', 'Dark Matter', 'Cybernetics'];
+  PI: number = Math.PI;
 }
